2843fc1fb749db4cf06eaefc8571944054e4dbc4
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-09-05 14:20:51 +0200

    Switch from node-gyp to cmake-js

--- src/module.cpp
+++ src/module.cpp
@@ -1,2 +1,10 @@
 
 #include <napi.h>
+#include <uv.h>
+
+void Init(Napi::Env env, Napi::Object exports, Napi::Object module)
+{
+    exports.Set(Napi::String::New(env, "hello"), Napi::String::New(env, "world"));
+}
+
+NODE_API_MODULE(vefs, Init)
