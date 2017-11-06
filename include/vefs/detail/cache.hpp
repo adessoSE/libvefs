412a496a3e41a5622939cac2b89072e870a9880f
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-11-06 10:06:56 +0100

    Allow external pool cache elements

--- include/vefs/detail/cache.hpp
+++ include/vefs/detail/cache.hpp
@@ -1,31 +1,39 @@
 #pragma once
 
 #include <memory>
+#include <functional>
 #include <type_traits>
 
-#include <vefs/detail/string_map.hpp>
-
+#include <vefs/ext/libcuckoo/cuckoohash_map.hh>
 #include <vefs/ext/concurrentqueue/concurrentqueue.h>
 
 namespace vefs::detail
 {
-    template <typename T>
+    template <class Key, class T, class Hash = std::hash<Key>,
+          class KeyEqual = std::equal_to<Key>,
+          class Allocator = std::allocator<std::pair<const Key, T>>,
+          std::size_t SLOT_PER_BUCKET = LIBCUCKOO_DEFAULT_SLOT_PER_BUCKET>
     class cache
     {
     public:
-        using id_type = shared_string;
-        using handle = std::shared_ptr<T>;
+        using key_type = Key;
+        using value_type = T;
+        using handle = std::shared_ptr<value_type>;
         using weak_handle = typename handle::weak_type;
+
+        using is_dirty_fn = std::function<bool(handle)>;
+        using on_free_fn = std::function<void(key_type &, handle)>;
 
     private:
         struct node
         {
-            node(cache *owner, id_type id, handle object)
+            node(cache *owner, key_type id, handle object)
                 : owner(owner)
-                , id(std::move(id))
+                , id(id)
                 , cached_object(std::move(object))
             {
             }
+            node(const node &) = delete;
             ~node()
             {
                 if (owner)
@@ -33,23 +41,52 @@ namespace vefs::detail
                     owner->on_node_destruction(id);
                 }
             }
+
+            node & operator=(const node &) = delete;
+
             void on_owner_released()
             {
                 owner = nullptr;
             }
 
             cache *owner;
-            id_type id;
             handle cached_object;
+            key_type id;
+        };
+        struct augmented_node
+            : node
+        {
+            augmented_node(cache *owner, key_type id, handle object, on_free_fn onFree)
+                : node(owner, std::move(id), std::move(object))
+                , on_free(std::move(onFree))
+            {
+            }
+            ~augmented_node()
+            {
+                if (on_free && owner)
+                {
+                    on_free(id, std::move(cached_object));
+                }
+            }
+
+            on_free_fn on_free;
         };
         using node_ptr = std::shared_ptr<node>;
         using node_weak_ptr = std::weak_ptr<node>;
 
 
         using access_queue = moodycamel::ConcurrentQueue<node_ptr>;
-        using cache_map = string_map<node_weak_ptr>;
+        using cache_map = cuckoohash_map<Key, node_weak_ptr, Hash, KeyEqual, Allocator, SLOT_PER_BUCKET>;
 
     public:
+        cache(is_dirty_fn isDirtyPredicate)
+            : is_dirty(std::move(isDirtyPredicate))
+        {
+            if (!is_dirty)
+            {
+                BOOST_THROW_EXCEPTION(logic_error{});
+            }
+        }
         ~cache()
         {
             // this MUST be done HERE explicitly, otherwise the invocation of on_node_destruction()
@@ -57,7 +94,7 @@ namespace vefs::detail
             clear();
         }
 
-        handle try_access(const id_type &id)
+        handle try_access(const key_type &id)
         {
             handle h;
             node_ptr n;
@@ -75,16 +112,77 @@ namespace vefs::detail
 
         void make_room()
         {
-            node_ptr dummy;
-            mAccessQueue.try_dequeue(dummy);
+            for (std::size_t i = 0, limit = mAccessQueue.size_approx(); i < limit; ++i)
+            {
+                node_ptr elem;
+                mAccessQueue.try_dequeue(elem);
+                if (elem && is_dirty(elem->cached_object) && elem.use_count() == 1)
+                {
+                    mAccessQueue.enqueue(std::move(elem));
+                }
+                else
+                {
+                    node_weak_ptr postMortem{ elem };
+                    elem.reset();
+                    // check wether the above reset killed the kid
+                    if (postMortem.expired())
+                    {
+                        break;
+                    }
+                }
+            }
         }
 
-        std::tuple<bool, handle> try_push(id_type id, handle obj)
+        std::tuple<bool, handle> try_push(key_type id, handle obj)
+        {
+            return try_push(
+                std::make_shared<node>(this, std::move(id), std::move(obj))
+            );
+        }
+
+        std::tuple<bool, handle> try_push(key_type id, handle obj, on_free_fn onFree)
+        {
+            return try_push(
+                std::static_pointer_cast<node>(
+                    std::make_shared<augmented_node>(this, std::move(id), std::move(obj), std::move(onFree))
+                )
+            );
+        }
+
+        void purge(const key_type &id)
+        {
+            mCachedValues.erase(id);
+        }
+        void purge(const key_type &id, const handle &obj)
+        {
+            mCachedValues.erase_fn(id, [&obj](node_weak_ptr &wnode)
+            {
+                if (auto node = wnode.lock(); node && node->cached_object != obj)
+                {
+                    // the key got overwritten, so we don't erase the new obj
+                    return false;
+                }
+                return true;
+            });
+        }
+
+        // this is a semi thread safe function
+        // while the cache will always stay in a valid state, the behaviour of accessing
+        // the cache during a clear operation can only be described as _undesirable_
+        void clear()
+        {
+            while (!mCachedValues.empty())
+            {
+                make_room();
+                std::this_thread::yield();
+            }
+        }
+
+    private:
+        std::tuple<bool, handle> try_push(node_ptr n)
         {
             bool existing = false;
-            auto n = std::make_shared<node>(this, std::move(id), std::move(obj));
-
-            mCachedValues.uprase_fn(std::move(id), [&existing, &n](node_weak_ptr &mapNode)
+            mCachedValues.uprase_fn(n->id, [&existing, &n](node_weak_ptr &mapNode)
             {
                 if (auto existingNode = mapNode.lock())
                 {
@@ -105,7 +203,7 @@ namespace vefs::detail
             return { existing, resource };
         }
 
-        void on_node_destruction(id_type node_id)
+        void on_node_destruction(key_type node_id)
         {
             mCachedValues.erase_fn(node_id, [](node_weak_ptr &n)
             {
@@ -117,20 +215,8 @@ namespace vefs::detail
             });
         }
 
-        // this is a semi thread safe function
-        // while the cache will always stay in a valid state, the behaviour of
-        // accessing cash during a clear operation can only be described as _undesirable_
-        void clear()
-        {
-            while (!mCachedValues.empty())
-            {
-                node_ptr n;
-                mAccessQueue.try_dequeue(n);
-            }
-        }
-
-    private:
         access_queue mAccessQueue;
         cache_map mCachedValues;
+        is_dirty_fn is_dirty;
     };
 }
--- include/vefs/detail/pool_cache.hpp
+++ include/vefs/detail/pool_cache.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 
+#include <thread>
 #include <utility>
 
 #include <vefs/detail/cache.hpp>
@@ -9,33 +10,74 @@
 
 namespace vefs::detail
 {
-    template <typename T, std::size_t NumChunks>
+    template <typename Cache, std::size_t NumChunks>
     class caching_object_pool
     {
-        using cache_t = cache<T>;
-        using pool_t = shared_object_pool<T, NumChunks>;
+        using cache_t = Cache;
+        using pool_t = shared_object_pool<typename cache_t::value_type, NumChunks>;
 
     public:
+        using key_type = typename cache_t::key_type;
+        using value_type = typename cache_t::value_type;
         using handle = typename cache_t::handle;
 
-        template <typename Fn, typename... ConstructorArgs>
-        std::tuple<bool, handle> access(const detail::shared_string &blockId, Fn &&initFn, ConstructorArgs... ctorArgs)
+        using is_dirty_fn = typename cache_t::is_dirty_fn;
+
+        caching_object_pool(is_dirty_fn isDirtyPredicate)
+            : mCache(std::move(isDirtyPredicate))
         {
-            if (auto cache_ptr = mCache.try_access(blockId))
+        }
+
+        template <typename Fn, typename... ConstructorArgs>
+        std::tuple<bool, handle> access(const key_type &key, Fn &&initFn, ConstructorArgs... ctorArgs)
+        {
+            return try_access<std::numeric_limits<std::size_t>::max()>(key, std::forward<Fn>(initFn),
+                std::move(ctorArgs)...);
+        }
+
+        handle try_access(const key_type &key)
+        {
+            return mCache.try_access(key);
+        }
+
+        template <std::size_t maxTries, typename Fn, typename... ConstructorArgs>
+        std::tuple<bool, handle> try_access(const key_type &key, Fn &&initFn, ConstructorArgs... ctorArgs)
+        {
+            if (auto cache_ptr = mCache.try_access(key))
             {
                 return { true, cache_ptr };
             }
             else
             {
-                auto block = mStorage.create(ctorArgs...);
-                while (!block)
+                auto elem = mStorage.create(ctorArgs...);
+                [[maybe_unused]] auto tries = maxTries;
+                while (!elem)
                 {
+                    std::this_thread::yield();
                     mCache.make_room();
-                    block = mStorage.create(ctorArgs...);
+                    elem = mStorage.create(ctorArgs...);
+
+                    if constexpr (maxTries)
+                    {
+                        if (!--tries)
+                        {
+                            return { false, {} };
+                        }
+                    }
                 }
-                initFn(*block);
-                return mCache.try_push(blockId, std::move(block));
+                initFn(*elem);
+                return mCache.try_push(key, std::move(elem));
             }
+        }
+
+        std::tuple<bool, handle> try_push_external(key_type key, handle obj,
+            typename cache_t::on_free_fn freeFn)
+        {
+            return mCache.try_push(std::move(key), std::move(obj), std::move(freeFn));
+        }
+        void purge(const key_type &key, const handle &obj)
+        {
+            mCache.purge(key, obj);
         }
 
     private:
