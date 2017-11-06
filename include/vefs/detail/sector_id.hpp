7fd5087c3874e5740a2603ed26d61084b9b22cff
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-11-06 10:08:45 +0100

    Improve sector id to string algorithms
    Use snprintf instead of iostreams for simple formatting.

--- include/vefs/detail/sector_id.hpp
+++ include/vefs/detail/sector_id.hpp
@@ -17,16 +17,9 @@ namespace vefs::detail
         master = 0,
     };
 
-    /*
-    inline bool operator==(sector_id lhs, sector_id rhs)
-    {
-        return static_cast<std::uint64_t>(lhs) == static_cast<std::uint64_t>(rhs);
-    }
-    inline bool operator!=(sector_id lhs, sector_id rhs)
-    {
-        return !(lhs == rhs);
-    }
-    */
+    constexpr std::size_t sector_id_string_size
+        = 2 + (std::numeric_limits< std::underlying_type_t<sector_id> >::digits >> 2);
+    constexpr const char *sector_id_string_fmt = "0x%.16llx";
 
     inline bool operator<(sector_id lhs, sector_id rhs)
     {
@@ -51,15 +44,40 @@ namespace vefs::detail
             << hexPrefix0 << hexPrefix1 << static_cast<raw_type>(sid);
     }
 
+    inline void to_string(sector_id id, std::string &out, std::size_t position = {})
+    {
+        char saved;
+        const auto size = position + sector_id_string_size;
+        bool append = out.size() <= size;
+        if (append)
+        {
+            out.resize(size + 1, '\0');
+        }
+        else
+        {
+            saved = out[size];
+        }
+
+        snprintf(out.data() + position, out.size() - position,
+            sector_id_string_fmt, static_cast<std::underlying_type_t<sector_id>>(id));
+
+        if (append)
+        {
+            out.pop_back();
+        }
+        else
+        {
+            out[size] = saved;
+        }
+    }
+
     inline std::string to_string(sector_id id)
     {
-        using raw_type = std::underlying_type_t<sector_id>;
-        using limits = std::numeric_limits<raw_type>;
-        constexpr auto hexDigits = limits::digits >> 2;
-        static_assert(hexDigits == 16);
+        std::string buffer(sector_id_string_size + 1, '\0');
 
-        std::string buffer(hexDigits + 3, '\0');
-        snprintf(buffer.data(), buffer.size(), "0x%.16llx", static_cast<raw_type>(id));
+        snprintf(buffer.data(), buffer.size(),
+            sector_id_string_fmt, static_cast<std::underlying_type_t<sector_id>>(id));
+
         buffer.pop_back(); // pop null terminator written by snprintf
         return buffer;
     }
