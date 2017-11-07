56134408fbf4efee207fe407e2c224fa9cac934a
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-11-07 08:07:33 +0100

    Precompile standard header files

--- src/precompiled.hpp
+++ src/precompiled.hpp
@@ -1,1 +1,31 @@
 #pragma once
+
+#if defined NDEBUG
+
+#include <cstddef>
+#include <cstdint>
+#include <cassert>
+
+#include <mutex>
+#include <tuple>
+#include <array>
+#include <atomic>
+#include <thread>
+#include <memory>
+#include <string>
+#include <limits>
+#include <utility>
+#include <optional>
+#include <stdexcept>
+#include <functional>
+#include <string_view>
+#include <type_traits>
+#include <system_error>
+
+#include <boost/predef.h>
+#include <boost/exception/all.hpp>
+
+#include <vefs/ext/libcuckoo/cuckoohash_map.hh>
+#include <vefs/ext/concurrentqueue/concurrentqueue.h>
+
+#endif
