bd8ac20098cc6e5656121848b67600c59cf0eee8
Henrik S. Gaßmann <henrik@gassmann.onl>
2018-03-08 08:33:01 +0100

    Add vefs cli project files
    - Require boost program_options

---
+++ src/cli/vefs-cli.cpp
@@ -0,0 +1,60 @@
+
+#include <cstddef>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <memory>
+#include <iostream>
+
+#include <boost/program_options.hpp>
+
+#include <vefs/archive.hpp>
+
+namespace bpo = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    using std::string;
+    using std::vector;
+
+    bpo::options_description globalopts{ "vefs-cli options" };
+    globalopts.add_options()
+        ("help", "outputs this useful help dialog")
+        ("archive-path,a", bpo::value<string>(), "the path to the archive")
+        ("command", bpo::value<string>(), "archive command to execute")
+        ("cmdargs", bpo::value<vector<string> >(), "arguments for command");
+    ;
+
+    bpo::positional_options_description posargs;
+    posargs
+        .add("archive-path", 1)
+        .add("command", 1)
+        .add("cmdargs", -1);
+
+    bpo::variables_map vm;
+
+    auto opts = bpo::command_line_parser(argc, argv)
+        .options(globalopts)
+        .positional(posargs)
+        .allow_unregistered()
+        .run();
+
+    bpo::store(opts, vm);
+
+    if (vm.count("command") && vm.count("archive-path"))
+    {
+        auto fs = vefs::os_filesystem();
+        auto cprov = vefs::crypto::boringssl_aes_256_gcm_crypto_provider();
+
+        auto cmd = vm["command"].as<std::string>();
+        auto apath = vm["archive-path"].as<std::string>();
+        std::array<std::byte, 32> prk{};
+        if (cmd == "create")
+        {
+            auto ac = std::make_unique<vefs::archive>(fs, apath,
+                cprov, vefs::blob_view{ prk }, vefs::archive::create);
+        }
+    }
+    return 0;
+}
