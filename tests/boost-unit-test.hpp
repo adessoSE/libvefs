e18bd6bd9aee3b3d9ec4e9c793f30b3d88e286cf
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2019-03-29 14:39:40 +0100

    Suppress boost test framework msvc warnings

--- tests/boost-unit-test.hpp
+++ tests/boost-unit-test.hpp
@@ -10,10 +10,20 @@
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #endif
 
+#if defined BOOST_COMP_MSVC_AVAILABLE
+#pragma warning(push, 2)
+#pragma warning(disable: 4702)
+#endif
+
+
 #include <boost/test/unit_test.hpp>
 #include <boost/test/parameterized_test.hpp>
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/data/monomorphic.hpp>
+
+#if defined BOOST_COMP_MSVC_AVAILABLE
+#pragma warning(pop)
+#endif
 
 #if defined BOOST_COMP_GNUC_AVAILABLE
 #pragma GCC diagnostic pop
