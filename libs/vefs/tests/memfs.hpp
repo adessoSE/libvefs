62ddd48c690f4f1802422ddf9b5983e53ed0f5fd
Henrik S. Gaßmann <henrik@gassmann.onl>
2019-03-31 13:40:18 +0200

    Manually allocate memfs chunks on the heap
    Store an owning chunk handle in the chunk list instead of the chunk
    itself. MSVC 19.20 apparently changed the std::list implementation to
    allocate a list element on the stack when resizing which leads to stack
    overflows in case of 1MiB byte arrays.

--- libs/vefs/tests/memfs.hpp
+++ libs/vefs/tests/memfs.hpp
@@ -2,11 +2,11 @@
 
 #include <cstddef>
 
-#include <list>
 #include <array>
-#include <vector>
+#include <memory>
 #include <mutex>
 #include <system_error>
+#include <list>
 
 #include <vefs/exceptions.hpp>
 #include <vefs/filesystem.hpp>
@@ -24,48 +24,73 @@ namespace vefs::tests
         file_not_found,
         out_of_memory,
     };
-    const std::error_category & memvefs_category();
+    const std::error_category &memvefs_category();
     inline std::error_code make_error_code(memvefs_code code)
     {
-        return { static_cast<int>(code), memvefs_category() };
+        return {static_cast<int>(code), memvefs_category()};
     }
-}
+} // namespace vefs::tests
+
 namespace std
 {
     template <>
-    struct is_error_code_enum<vefs::tests::memvefs_code>
-        : std::true_type
+    struct is_error_code_enum<vefs::tests::memvefs_code> : std::true_type
     {
     };
-}
+} // namespace std
 
 namespace vefs::tests
 {
     struct memory_filesystem;
 
-    struct memory_file
-        : public file
+    struct memory_file : public file
     {
         struct memory_holder
         {
             static constexpr std::size_t chunk_size = 1 << 20;
 
-            using chunk = std::array<std::byte, chunk_size>;
-            using chunk_list = std::list<chunk>;
+            struct chunk_handle
+            {
+                using chunk = std::array<std::byte, chunk_size>;
+                using chunk_ptr = std::unique_ptr<chunk>;
+
+                chunk_handle()
+                    : mData(std::make_unique<chunk>())
+                {
+                }
+                chunk_handle(chunk_handle &&o) noexcept
+                    : mData(std::move(o.mData))
+                {
+                }
+                chunk_handle &operator=(chunk_handle &&o) noexcept
+                {
+                    mData = std::move(o.mData);
+                }
+
+                blob data() noexcept
+                {
+                    return blob{*mData};
+                }
+
+            private:
+                chunk_ptr mData;
+            };
+
+            using chunk_list = std::list<chunk_handle>;
             using lock_t = std::lock_guard<std::mutex>;
 
             memory_holder()
                 : mGrowMutex{}
                 , mChunks{}
-                , mCurrentSize{ 0 }
-                , mMaxSize{ std::numeric_limits<std::size_t>::max() }
+                , mCurrentSize{0}
+                , mMaxSize{std::numeric_limits<std::size_t>::max()}
             {
                 mChunks.emplace_back();
             }
 
             void resize(std::size_t size)
             {
-                lock_t sync{ mGrowMutex };
+                lock_t sync{mGrowMutex};
                 // this allows to simulate sparse disk space conditions
                 if (size > mMaxSize)
                 {
@@ -84,7 +109,7 @@ namespace vefs::tests
                         const auto fraction = size % chunk_size;
                         if (fraction)
                         {
-                            utils::secure_memzero(blob{ mChunks.back() }.slice(fraction));
+                            utils::secure_memzero(mChunks.back().data().slice(fraction));
                         }
                     }
                     mCurrentSize = size;
@@ -92,7 +117,7 @@ namespace vefs::tests
             }
             std::size_t size() const
             {
-                lock_t sync{ mGrowMutex };
+                lock_t sync{mGrowMutex};
                 return mCurrentSize;
             }
 
@@ -108,7 +133,7 @@ namespace vefs::tests
                 using iter_t = chunk_list::iterator;
                 iter_t it;
                 {
-                    lock_t sync{ mGrowMutex };
+                    lock_t sync{mGrowMutex};
                     it = mChunks.begin();
                 }
                 const auto beginIdx = offset / chunk_size;
@@ -120,19 +145,19 @@ namespace vefs::tests
                 if (endIdx - beginIdx <= 1)
                 {
                     // the area to be processed is contained within the same chunk
-                    fn(blob{ *it }.slice(offset % chunk_size, size));
+                    fn(it->data().slice(offset % chunk_size, size));
                 }
                 else
                 {
                     iter_t end = it;
                     std::advance(end, endIdx - beginIdx - 1);
 
-                    fn(blob{ *it++ }.slice(offset % chunk_size));
+                    fn((it++)->data().slice(offset % chunk_size));
                     for (; it != end; ++it)
                     {
-                        fn(blob{ *it });
+                        fn(it->data());
                     }
-                    fn(blob{ *it }.slice(0, (offset + size) % chunk_size));
+                    fn(it->data().slice(0, (offset + size) % chunk_size));
                 }
             }
 
@@ -142,24 +167,27 @@ namespace vefs::tests
             std::size_t mMaxSize;
         };
 
-        memory_file(std::shared_ptr<memory_filesystem> owner, std::shared_ptr<memory_holder> memory, file_open_mode_bitset mode)
+        memory_file(std::shared_ptr<memory_filesystem> owner, std::shared_ptr<memory_holder> memory,
+                    file_open_mode_bitset mode)
             : mOwner(owner)
             , mMemory(memory)
             , mOpenMode(mode)
         {
         }
 
-        virtual void read(blob buffer, std::uint64_t readFilePos, std::error_code & ec) override;
-        virtual std::future<void> read_async(blob buffer, std::uint64_t readFilePos, async_callback_fn callback) override;
+        virtual void read(blob buffer, std::uint64_t readFilePos, std::error_code &ec) override;
+        virtual std::future<void> read_async(blob buffer, std::uint64_t readFilePos,
+                                             async_callback_fn callback) override;
 
-        virtual void write(blob_view data, std::uint64_t writeFilePos, std::error_code & ec) override;
-        virtual std::future<void> write_async(blob_view data, std::uint64_t writeFilePos, async_callback_fn callback) override;
+        virtual void write(blob_view data, std::uint64_t writeFilePos, std::error_code &ec) override;
+        virtual std::future<void> write_async(blob_view data, std::uint64_t writeFilePos,
+                                              async_callback_fn callback) override;
 
-        virtual void sync(std::error_code & ec) override;
+        virtual void sync(std::error_code &ec) override;
         virtual std::future<void> sync_async(async_callback_fn callback) override;
 
-        virtual std::uint64_t size(std::error_code & ec) override;
-        virtual void resize(std::uint64_t newSize, std::error_code & ec) override;
+        virtual std::uint64_t size(std::error_code &ec) override;
+        virtual void resize(std::uint64_t newSize, std::error_code &ec) override;
         virtual std::future<void> resize_async(std::uint64_t newSize, async_callback_fn callback) override;
 
         std::shared_ptr<memory_filesystem> mOwner;
@@ -168,11 +196,10 @@ namespace vefs::tests
     };
 
     struct memory_filesystem
-        : public filesystem
+        : public vefs::filesystem
         , std::enable_shared_from_this<memory_filesystem>
     {
-        using relaxed_string_map
-            = utils::unordered_string_map_mt<std::shared_ptr<memory_file::memory_holder>>;
+        using relaxed_string_map = utils::unordered_string_map_mt<std::shared_ptr<memory_file::memory_holder>>;
 
         static std::shared_ptr<memory_filesystem> create()
         {
@@ -180,9 +207,9 @@ namespace vefs::tests
         }
 
         using filesystem::open;
-        virtual file::ptr open(std::string_view filePath, file_open_mode_bitset mode, std::error_code & ec) override;
+        virtual file::ptr open(std::string_view filePath, file_open_mode_bitset mode, std::error_code &ec) override;
         virtual void remove(std::string_view filePath) override;
 
         relaxed_string_map files;
     };
-}
+} // namespace vefs::tests
