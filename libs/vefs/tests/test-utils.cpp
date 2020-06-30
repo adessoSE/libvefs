d4dd7440658db233fee100b653e62de30192a325
Böttinger, Torsten <torsten.boettinger@adesso.de>
2020-06-30 12:09:51 +0000

    rework archive-tests

---
+++ libs/vefs/tests/test-utils.cpp
@@ -0,0 +1,10 @@
+#include "test-utils.hpp"
+namespace adesso::vefs
+{
+    std::ostream &operator<<(std::ostream &os,
+                             const adesso::vefs::FileDescriptor &ref)
+    {
+        os << "Forward declared ref: \n";
+        return os;
+    }
+} // namespace adesso::vefs
