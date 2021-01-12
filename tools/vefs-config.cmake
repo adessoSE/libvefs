ecfb7f4ddd44a035264c475243ff8cc12b963692
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2021-01-12 10:10:34 +0100

    Include transitive dependencies in cmake config

--- tools/vefs-config.cmake
+++ tools/vefs-config.cmake
@@ -1,1 +1,19 @@
+
+include(CMakeFindDependencyMacro)
+
+find_dependency(outcome CONFIG)
+find_dependency(llfio CONFIG)
+
+find_dependency(Boost COMPONENTS
+    system
+)
+
+find_dependency(libb2 CONFIG)
+
+find_dependency(boringssl CONFIG)
+
+find_dependency(deeppack CONFIG)
+
+find_dependency(fmt CONFIG)
+
 include("${CMAKE_CURRENT_LIST_DIR}/vefs-targets.cmake")
