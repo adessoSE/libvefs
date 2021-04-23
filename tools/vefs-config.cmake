49871f0d14ba30f06e8fa8580fff1ba165846759
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2021-04-23 11:58:10 +0200

    Find more transitive dependencies during import config

--- tools/vefs-config.cmake
+++ tools/vefs-config.cmake
@@ -8,12 +8,16 @@ find_dependency(Boost COMPONENTS
     system
 )
 
+find_dependency(boringssl CONFIG)
+
 find_dependency(libb2 CONFIG)
 
-find_dependency(boringssl CONFIG)
+find_dependency(libcuckoo CONFIG REQUIRED)
 
 find_dependency(deeppack CONFIG)
 
 find_dependency(fmt CONFIG)
+
+find_dependency(unofficial-concurrentqueue CONFIG)
 
 include("${CMAKE_CURRENT_LIST_DIR}/vefs-targets.cmake")
