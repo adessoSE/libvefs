bcbb74b7807bd324c64e148306c3ead55164324a
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-02-14 08:53:21 +0100

    Suppress `llfio` MSVC warnings

--- libs/vefs/include/vefs/llfio.hpp
+++ libs/vefs/include/vefs/llfio.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <boost/predef/compiler.h>
+
+#if defined(BOOST_COMP_MSVC_AVAILABLE)
+#pragma warning(push, 2)
+#pragma warning(disable : 4905) // C4905: wide string literal cast to 'LPSTR'
+#pragma warning(disable : 4906) // C4906: string literal cast to 'LPWSTR'
+#endif
+
 #include <llfio/llfio.hpp>
+
+#if defined(BOOST_COMP_MSVC_AVAILABLE)
+#pragma warning(pop)
+#endif
 
 namespace vefs
 {
