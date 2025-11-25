2a58f675417f00f1ddb43a5a6a874f6aa4ae792a
Henrik Steffen Gaßmann <henrik@gassmann.onl>
2025-11-25 17:43:23 +0100

    build: open version 0.6.0-alpha.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2017-2023 adesso AG'
 author = 'Henrik Gaßmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0-alpha.1'
+release = '0.6.0-alpha.2'
 
 
 # -- General configuration ---------------------------------------------------
